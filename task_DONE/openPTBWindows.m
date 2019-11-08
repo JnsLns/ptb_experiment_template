9f256f7c6cb28fa25b4356e0ca6c47102135de7e
JnsLns <JLins@JLins.de>
2019-11-08 14:50:27 +0100

    Added warning in case saving is disabled.

---
+++ task_DONE/openPTBWindows.m
@@ -0,0 +1,64 @@
+%%%% Open PTB windows 
+
+% note: if certain images (e.g. fixation) are repeatedly used in your
+% experiment, make a separate offscreen window for each of them. This way
+% you need to draw that image once and afterwards only need to copy the
+% offscreen window to the onscreen window when the image is needed.
+%
+% The onscreen window handle is stored in struct winOn.h, as well as the
+% window's other properties, e.g., winOn.rect. Offscreen window handles are
+% stored as winsOff.windowname.h, and window properties in other fields,
+% such as% winsOff.windowname.rect. Additional offscreen windows should be
+% stored accordingly, e.g., winsOff.windowname2.h (etc.).
+
+% onscreen window for actual display
+winOn.bgColor = e.s.bgColor;
+winOn.screen = expScreen;
+winOn.rect = [0 0 e.s.expScreenSize_px];
+[winOn.center(1), winOn.center(2)] = RectCenter(winOn.rect);
+winOn.font = e.s.instructionTextFont;
+winOn.fontSize = vaToPx(e.s.instructionTextHeight_va, e.s.spatialConfig);
+[winOn.h, winOn.rect] = ...
+    PsychImaging('openWindow', winOn.screen, winOn.bgColor, winOn.rect);
+Screen('TextFont', winOn.h, winOn.font);
+Screen('TextSize', winOn.h, winOn.fontSize); 
+
+% offscreen window to draw stimuli to
+winsOff.stims.bgColor = e.s.bgColor;
+winsOff.stims.screen = expScreen;
+winsOff.stims.rect = winOn.rect;
+[winsOff.stims.center(1), winsOff.stims.center(2)] = ...
+    RectCenter(winsOff.stims.rect);
+[winsOff.stims.h, winsOff.stims.rect] = ...
+    Screen('OpenOffScreenWindow', winsOff.stims.screen, ...
+    winsOff.stims.bgColor, winsOff.stims.rect);
+
+% empty offscreen window
+winsOff.empty.bgColor = e.s.bgColor;
+winsOff.empty.screen = expScreen;
+winsOff.empty.rect = winOn.rect;
+[winsOff.empty.center(1), winsOff.empty.center(2)] = ...
+    RectCenter(winsOff.empty.rect);
+[winsOff.empty.h, winsOff.empty.rect] = ...
+    Screen('OpenOffScreenWindow', winsOff.empty.screen, ...
+    winsOff.empty.bgColor, winsOff.empty.rect);
+
+% offscreen window for fixation cross
+winsOff.fix.bgColor = e.s.bgColor;
+winsOff.fix.screen = expScreen;
+winsOff.fix.rect = winOn.rect;
+[winsOff.fix.center(1), winsOff.fix.center(2)] = ...
+    RectCenter(winsOff.fix.rect);
+[winsOff.fix.h, winsOff.fix.rect] = ...
+    Screen('OpenOffScreenWindow', winsOff.fix.screen, ...
+    winsOff.fix.bgColor, winsOff.fix.rect);
+
+% offscreen window for target presence response
+winsOff.targetResponse.bgColor = e.s.bgColor;
+winsOff.targetResponse.screen = expScreen;
+winsOff.targetResponse.rect = winOn.rect;
+[winsOff.targetResponse.center(1), winsOff.targetResponse.center(2)] = ...
+    RectCenter(winsOff.targetResponse.rect);
+[winsOff.targetResponse.h, winsOff.targetResponse.rect] = ...
+    Screen('OpenOffScreenWindow', winsOff.targetResponse.screen, ...
+    winsOff.targetResponse.bgColor, winsOff.targetResponse.rect);
--- task_DONE/randTask.m
+++ task_DONE/randTask.m
@@ -1,259 +1,12 @@
 %% Illusory conjunction task for motion tracker hardware
 % Jonas Lins, October 2019
 
+% See documentation.txt for help.
 
 
-% The general idea of this template code is that to create a new
-% experiment, functions and files in the private folder do not need to be
-% touched - they can be used as is. Paradigm-specific modifications should
-% be made only in experiment.m (adding settings, opening psychtoolbox
-% windows; things that need to be done before or after all trials),
-% singleTrial.m (sequence of events in each trial; getting responses;
-% specifying output variables to be stored), and drawStimuli.m (draw
-% stimulus items to psychtoolbox offscreen windows for presentation in
-% singleTrial.m).
-%
-%
-%                  _____Script input and output_____
-%
-%
-% Trial generation --> struct 'tg', stored in each trial file and loaded
-%                      in the current script anew for each participant.
-%                      Fields:
-%
-%                      tg.triallist     Matrix, rows are individual trials
-%                                       columns are trial properties
-%                      tg.s.triallistCols Struct, each of whose fields holds
-%                                       an integer that can be used to
-%                                       address into the columns of
-%                                       tg.triallist. Used to address trial
-%                                       properties "by name" instead of
-%                                       column number.
-%                      tg.s             Struct with various fields that hold
-%                                       settings for the experiment that
-%                                       apply across trials (e.g.,
-%                                       background color).
-%
-% Experiment (this script) --> struct 'e', stored in each results file
-%                              Fields:
-%
-%                       e.results       Matrix, each row corresponding to
-%                                       one presented trial, and columns
-%                                       pertaining to different response or
-%                                       trial properties.
-%                       e.s.resCols     Struct, function analogous to
-%                                       'tg.s.triallistCols' (see above)
-%                                       but for 'e.results'.
-%                       e.s             Struct with various fields that
-%                                       hold settings for the experiment
-%                                       (including copies of all fields of
-%                                       'tg.s', except 'triallistCols')
-%
-%
-%                     _____Specifying outputs_____
-%
-% SUMMARY: Everything written to fields of struct 'out' during trials will
-% autoimatically be stored it in the results matrix ('e.results'). Add
-% fields to 'out' as needed, but assign only integers or column vectors.
-% Columns of the results matrix can later be addressed "by name" using
-% column indices in fields of 'e.s.resCols'. 
-%
-% DETAILS: An empty struct 'out' is created/reset before each trial. Fields
-% can be created in this struct as needed during the trial (preferably in
-% singleTrial.m); a column vector or an integer should be written to each
-% field.
-% When a trial ends, the objects stored in the fields of 'out' will be
-% transformed into a row of results values and this row is written to the
-% results matrix ('e.results').
-%
-% To be able to address into the columns of 'e.results' later, a struct
-% 'e.s.resCols' is automatically created. Its fields hold integers that
-% are column indices into 'e.results', allowing to address the columns
-% "by name". The fields of e.s.resCols are named after the fields that
-% exist in 'out' at the end of a trial. For instance, 
-% 
-% >> out.RT = 1150;                 % say this happens in the 10th trial.
-%
-% >> e.s.resCols.RT                 % later check what column number has
-% ans = 12                          % been automatically assigned to RT.
-% 
-% >> e.results(10, e.s.resCols.RT)  % it can be used during analysis to
-% ans = 1150                        % address into the results matrix.       
-%
-% If the value of a field of 'out' is a column vector then its elements
-% will be written to the results matrix in consecutive columns and
-% e.s.resCols will have two fields for this object, one to address into the
-% first column of the span of values, the other addressing into the last
-% column. These two fields are named the same as the respective field of
-% 'out' except for being postfixed with 'Start' and 'End', respectively.
-% For instance,
-% 
-% >> out.foo = [1,2,3];             % say this happens in the 10th trial.
-%
-% >> e.s.resCols.fooStart
-% ans = 12
-% >> e.s.resCols.fooEnd   
-% and = 14
-%
-% >> e.results(10, e.s.resCols.fooStart:e.s.resCols.fooEnd)                                    
-% ans = [1,2,3]
-%
-% The absolute values of the column indices depend on how many fields have
-% been created before. It is possible to add fields to 'out' that were not
-% used in earlier trials, in which case rows in e.results from preceding
-% trials will be filled with nans in these columns. Note however that once
-% a field has been used, the objects stored in it must be of the same size
-% in each trial (pad with nans if needed).
+%%%% Settings
 
-%
-%                   _____Experiment settings_____
-%
-% Most settings for the experiment have to be defined already during trial
-% generation and are assumed to be found in struct 'tg.s' loaded from a
-% trial file. This ensures that once trials have been generated and saved,
-% experiment settings are not changed accidently halfway through
-% participants. Note that all fields found in 'tg.s' will be copied to
-% 'e.s' here (except for field 'triallistCols') so that 'tg' will not be
-% needed for analysis.
-%
-% There are only a few settings that must be set here in the experimental
-% script itself (these are as well stored in 'e.s'), namely those that
-% pertain to specific properties of the hardware used or the spatial
-% arrangement, such as screen size or viewing distance of the participant.
-% These are set here in order to allow things like switching to a different
-% screen halfway through participants. The actual sizes of stimuli on
-% the screen as well as other spatial properties are computed dynamically
-% in the current script based on these settings, so that all distances and
-% sizes will automatically adhere to the desired visual angle values
-% specified in the trial list.
-%
-%
-%
-%                    _____Spatial reference frames_____
-%
-% SUMMARY: All input and output to and from this script is in the
-% presentation-area-based frame and in degrees visual angle EXCEPT for
-% trajectory data (which is in the same frame but in millimeters) and very
-% few settings that make sense only in millimeters (like physical screen
-% size; these are postfixed '_mm') or pixels (like screen resolution;
-% postfixed '_px'). 
-%
-% DETAILS: There are three spatial reference frames used here:
-%
-% -- Presentation-area-based-frame (pa):
-% 
-% Used for all input to and output from this script, that is, in trial
-% generation (struct 'tg' loaded from trial file) and for results output,
-% that is, everything that is stored in the output struct 'e'. The origin
-% is at the bottom left of the presentation area (rectangular area whose
-% size is defined in 'tg' and which is inset in and centered within the
-% screen), x-axis increases to the right, y-axis increases upward. Units
-% used in conjunction with this frame are degrees of visual angle (va),
-% except for very few exceptions, which are: (1) Trajectory data (in
-% e.trajectories), which uses this frame but is in millimeters (as visual
-% angle does not make much sense for 3D data; the positive z-axis extends
-% toward the viewer and is orthogonal to the screen surface) and (2) a few
-% settings that make sense only in millimeters (these settings are clearly
-% marked by the postfix '_mm' in the fieldname in struct 'e.s').
-%
-% -- Psychtoolbox frame (ptb):
-%
-% Used *only* in the internals of the trial script (singleTrial.m) when
-% drawing things to the screen via Psychtoolbox. The origin is at the top
-% left of the screen, x-axis increasing to the right, y-axis increasing
-% downward. Units used in conjunction with this frame are pixels.
-%
-% -- Screen-marker-based frame (scr):
-%
-% This is the frame in which pointer position is obtained from function
-% getTip_pa (see private folder) during the experiment.
-% (Apart from a prespecified offset, as explained below) its origin is at
-% the position of the marker mounted at the lower left of the screen's
-% visible image area, its positive x-axis points in the direction
-% of the marker mounted at the lower right corner of the visible image
-% area, the positive x-y-plane is defined by the marker on the top border
-% of the visible image (at a positive x-value). The z-axis extends toward
-% the viewer. As hinted above, this frame (i.e., what is obtained from
-% getTip_pa) is offset in x/y/z direction by the three-element vector in
-% e.s.markerCRFoffset_xyz_mm, the main use of which is to shift the
-% x-y-plane(z=0) right onto the screen surface despite marker diodes being
-% mounted somewhat above the screen surface.
-%
-%
-%
-%                 ____CRF/unit conversion functions____
-%
-% The private folder contains functions that allow converting between the
-% different reference frames, by passing the values to be converted 
-% plus the struct 'e.s.spatialConfig' (which holds all relevant information
-% about the spatial setup, such as viewing distance, screen extent etc).
-% All these functions follow the same naming scheme (see the individual
-% functions' documentations for more info). Naming examples:
-%
-% paVaToPtbPx  -->  convert from presentation-area-based frame (pa) in
-%                   degrees visual angle (va) to Psychtoolbox frame (ptb)
-%                   in pixels (px).
-%
-% scrMmToPaMm  -->  convert from screen-marker-based frame (scr) in
-%                   millimeters (mm) to presentation-area-based frame (pa)
-%                   in millimeters (mm).
-%
-% pxToMm       -->  convert from pixels (px) to millimeters (mm).
-%
-%
-%
-%                 _____Conditions, responses & codes_____
-%
-% Conditions, responses, and response types, plus corresponding column
-% names and codes used in 'e.results':
-%
-%                 TRIAL TYPE     PRESENT RESPONSE       RESPONSE TYPE
-% -------------------------------------------------------------------------
-% e.s.resCols     .trialType   .tgtPresentResponse      .responseType  
-% -------------------------------------------------------------------------
-%              tgt present (1) | tgt present (1) | hit                  (1)
-%              tgt present (1) | tgt absent  (0) | miss                 (2)
-%              both present(2) | tgt present (1) | illusory conjunction (3)
-%              both present(2) | tgt absent  (0) | correct rejection BP (4)
-%              color only  (3) | tgt present (1) | feature error        (5)
-%              color only  (3) | tgt absent  (0) | correct rejection CO (6)
-%
-%
-%
-%                   _____Trial abortion & codes_____
-%
-% Trials may be aborted before completion due to various reasons. Any data
-% pertaining to aborted trials will nonetheless be stored in e.results,
-% including an abort code (column number is e.s.resCols.abortCode) giving
-% the reason for abortion. These are:
-%
-% 0     trial completed
-% 1     not moved to starting position in time
-% 2     moved off starting position during fixation phase
-% 3     moved off starting position during stimulus presentation
-% 4     exceeded max RT for location response
-% 5     exceeded max RT for target presence response
-%
-% Any missing results values for prematurely aborted trials (e.g., response
-% correctness) will be nan in 'e.results' and cells in 'e.trajectories' will
-% contain an empty matrix if recording has not started yet (if abortion
-% instead occurs during recording, an incomplete trajectory will be stored).
-
-
-
-%%%% Basic settings
-
-PsychDefaultSetup(2);               % call some settings for Psychtoolbox
-screens = Screen('Screens');        % get connected screens
-expScreen = max(screens);           % use last screen as stimulus display
-Screen('Preference', 'VisualDebuglevel', 3); % suppress PTB splash screen
-Screen('Preference','SkipSyncTests',1); % use when debugging without
-                                        % in windowed mode
-
-
-%%%% Settings needed for every paradigm
-
-% will be appended to each results file name
+% This will be appended to each results file name
 experimentName = 'my_exp';
 
 % actual screen size in mm (actual image area)
@@ -266,9 +19,12 @@ e.s.viewingDistance_mm = 500;
 % name of key to press to pause experiment (to pause, the key needs to be
 % depressed at the start of a trial)
 pauseKey = 'Pause';
-                                
-                    
-%%% Settings that are specific to the current paradigm
+       
+% save results to file?
+doSave = true;
+
+
+%%%% Settings specific to IC / trajectory-based paradigm
 
 % Define column numbers for trajectory matrices 
 e.s.trajCols.x = 1; % pointer coordinates
@@ -313,19 +69,43 @@ e.s.dontRecordConstantTrajData = 1;
 
 
                        %%%% END OF SETTINGS %%%%
+
+                       
+%%%% Warn in case saving is disabled
+                       
+if ~doSave   
+    resp = ...
+    questdlg('Saving is disabled, results will not be recorded.', 'Warning', ...
+                'Continue without saving','Abort', 2);
+    if strcmp(resp, 'Abort')
+        return
+    end    
+end
                        
 
 %%%% Load list of trials and settings from file
 
 [e.s.trialsFileName, trialsPath] = uigetfile('*.mat', 'Select trial file.');
 load([trialsPath, e.s.trialsFileName]);
- 
 
 
 %%%% Ask for save path
+if doSave
+    savePath = requestSavePath(experimentName);
+end
 
-savePath = requestSavePath(experimentName);
 
+%%%% Psychtoolbox settings
+                       
+PsychDefaultSetup(2);               % some default Psychtoolbox settings
+Screen('Preference', 'VisualDebuglevel', 3); % suppress PTB splash screen
+% Screen('Preference','SkipSyncTests',1); % use when debugging in windowed mode
+
+
+%%%% Determine experimental screen
+
+screens = Screen('Screens');        % get all connected screens
+expScreen = max(screens);           % use last screen as stimulus display
 
 
 %%%% Complete settings struct e.s.
@@ -357,12 +137,11 @@ for fn = fieldnames(tg.s)'
 end
 
 
-
-%%%% Trial data variables (used in rest of experiment)
+%%%% Trial data variables (remove tg... it won't be needed anymore)
 
 trials = tg.triallist;
 triallistCols = tg.s.triallistCols;
-
+clear tg; 
 
 
 %%%% Pointer Calibration
@@ -372,94 +151,33 @@ triallistCols = tg.s.triallistCols;
     doCalibrationProcedure(pad_tipID, pad_IDs, pointer_IDs);
 
 
-
 %%%% Initialize output arrays
 
 e.results = [];
 e.trajectories = cell();
 
 
-
 %%%% Create results file
 
-save(savePath, 'e');
+if doSave
+    save(savePath, 'e');
+end
 
 
-
-%%%% Conert color-defining field in e.s from strings to RGB
+%%%% ConVert color-defining field in e.s from strings to RGB
 
 colorDefinition;
 
 
-
 %%%% Open PTB windows & hide cursor
 
-% note: if certain images (e.g. fixation) are repeatedly used in your
-% experiment, make a separate offscreen window for each of them. This way
-% you need to draw that image once and afterwards only need to copy the
-% offscreen window to the onscreen window when the image is needed.
-%
-% The onscreen window handle is stored in struct winOn.h, as well as the
-% window's other properties, e.g., winOn.rect. Offscreen window handles are
-% stored as winsOff.windowname.h, and window properties in other fields,
-% such as% winsOff.windowname.rect. Additional offscreen windows should be
-% stored accordingly, e.g., winsOff.windowname2.h (etc.).
-
-% onscreen window for actual display
-winOn.bgColor = e.s.bgColor;
-winOn.screen = expScreen;
-winOn.rect = [0 0 e.s.expScreenSize_px];
-[winOn.center(1), winOn.center(2)] = RectCenter(winOn.rect);
-winOn.font = e.s.instructionTextFont;
-winOn.fontSize = vaToPx(e.s.instructionTextHeight_va, e.s.spatialConfig);
-[winOn.h, winOn.rect] = ...
-    PsychImaging('openWindow', winOn.screen, winOn.bgColor, winOn.rect);
-Screen('TextFont', winOn.h, winOn.font);
-Screen('TextSize', winOn.h, winOn.fontSize); 
-
-% offscreen window to draw stimuli to
-winsOff.stims.bgColor = e.s.bgColor;
-winsOff.stims.screen = expScreen;
-winsOff.stims.rect = winOn.rect;
-[winsOff.stims.center(1), winsOff.stims.center(2)] = ...
-    RectCenter(winsOff.stims.rect);
-[winsOff.stims.h, winsOff.stims.rect] = ...
-    Screen('OpenOffScreenWindow', winsOff.stims.screen, ...
-    winsOff.stims.bgColor, winsOff.stims.rect);
-
-% empty offscreen window
-winsOff.empty.bgColor = e.s.bgColor;
-winsOff.empty.screen = expScreen;
-winsOff.empty.rect = winOn.rect;
-[winsOff.empty.center(1), winsOff.empty.center(2)] = ...
-    RectCenter(winsOff.empty.rect);
-[winsOff.empty.h, winsOff.empty.rect] = ...
-    Screen('OpenOffScreenWindow', winsOff.empty.screen, ...
-    winsOff.empty.bgColor, winsOff.empty.rect);
-
-% offscreen window for fixation cross
-winsOff.fix.bgColor = e.s.bgColor;
-winsOff.fix.screen = expScreen;
-winsOff.fix.rect = winOn.rect;
-[winsOff.fix.center(1), winsOff.fix.center(2)] = ...
-    RectCenter(winsOff.fix.rect);
-[winsOff.fix.h, winsOff.fix.rect] = ...
-    Screen('OpenOffScreenWindow', winsOff.fix.screen, ...
-    winsOff.fix.bgColor, winsOff.fix.rect);
-
-% offscreen window for target presence response
-winsOff.targetResponse.bgColor = e.s.bgColor;
-winsOff.targetResponse.screen = expScreen;
-winsOff.targetResponse.rect = winOn.rect;
-[winsOff.targetResponse.center(1), winsOff.targetResponse.center(2)] = ...
-    RectCenter(winsOff.targetResponse.rect);
-[winsOff.targetResponse.h, winsOff.targetResponse.rect] = ...
-    Screen('OpenOffScreenWindow', winsOff.targetResponse.screen, ...
-    winsOff.targetResponse.bgColor, winsOff.targetResponse.rect);
+% create psychtoolbox onscreen and offscreen windows. NOTE: See this script
+% for info on how to create additional offscreen windows (it is useful to
+% create an offscreen window for displays that reoccur mutliple times.
+openPTBwindows;
 
 % Hide mouse cursor
 HideCursor;
-
 
 
 %%%% Things to be presented before the experiment
@@ -468,7 +186,6 @@ HideCursor;
 ShowTextAndWait(...
     'Bereit. Zum Starten des Experiments beliebige Taste drücken!', ...
     e.s.textColor, winOn.h, 0.5, true);
-
 
 
 %%%% Trial loop
@@ -514,7 +231,6 @@ while curTrial <= size(trials,1)
     curTrial = curTrial + 1; 
     
 end
-
 
 
 %%%% Things to be presented after the experiment
