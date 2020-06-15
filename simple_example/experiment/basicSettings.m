5c7c198b184cfbc04421fca2dab2b7ef106546fe
Jonas Lins <jlins@jlins.de>
2020-06-15 14:00:29 +0200

    minor changes in doc comments

--- simple_example/experiment/basicSettings.m
+++ simple_example/experiment/basicSettings.m
@@ -6,22 +6,19 @@
 % These are general settings needed for all experiments. In other words:
 % adjust their values, but don't remove variables.
 
-% Save results to file? If enabled, will ask for a participant code and
-% save path at the outset of the experiment. 
+% Save results to file? 
 doSave = true;
 
-% If a path name is given here, the result file will be saved there (the 
-% file name will be a combination of the participant code and the experiment
-% name defined during trial generation), without asking. Can be left empty
-% '' or commented out, in which case a folder select dialog will ask to
-% select a directory each time the experiment is started. Note: The variable
-% 'expRootDir' contains the path of the experiment root directory.
+% If a folder path is given here, the result file will be saved there
+% without asking. Can be left empty '' or commented out, in which case a
+% folder select dialog will ask to select a directory upon experiment start.
+% Note: variable 'expRootDir' contains the path of the experiment root dir.
 savePath = fullfile(expRootDir,'/../results');
 
 % If a path and name of a mat-file is given here, this file is loaded 
 % automatically as trial file. Can be left empty '' or commented out, in
-% which case a load dialog will ask to select a trial file each time the
-% experiment is started.
+% which case a load dialog will ask to select a trial file upon experiment
+% start.
 trialFilePath = '';
 
 % Number of screen to use as experimental screen. If this variable is not 
@@ -49,7 +46,8 @@ pauseKey = 'Pause';
 % on the desk, as arising from mouse system settings. Required for using
 % getMouseRM to stream/remap mouse movement (see Readme.md). No effect
 % otherwise. To determine this value for the current hardware, run
-% getMouseScreenToDesk(), located in /experiment/helperFunctions/mouseInput/.
+% getMouseScreenToDesk(), located in /experiment/infrastructure/
+% helperFunctions/mouseInput/.
 e.s.rawMouseScreenToDeskRatio = 48.3;
 
 
