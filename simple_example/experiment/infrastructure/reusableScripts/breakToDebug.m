e54a31368b809c4b57bc7f49c35b6eccdaebae9c
Jonas Lins <jlins@jlins.de>
2020-06-17 11:18:42 +0200

    minor doc adjustment

--- simple_example/experiment/infrastructure/reusableScripts/breakToDebug.m
+++ simple_example/experiment/infrastructure/reusableScripts/breakToDebug.m
@@ -1,6 +1,8 @@
 % Use for debugging. Essentially sets a break point at the position where 
 % the script is included but additionally closes any PTB windows and shows
-% the call stack. Resume code execution as usual when done debugging.
+% the call stack. Resume code execution as usual when done debugging. All
+% PTB windows will be restored (and static graphics re-drawn), and the
+% experiment will resume.
 
 Screen('CloseAll');
 dbstack;
